8f9b67a3a01f8130913de455c30e7920dcc7e151
Arnab0089 <arnabkundu756@gmail.com>
2023-11-08 19:44:10 +0530

    Navbar modification

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,25 +1,52 @@
 import React from 'react';
 import '../Navbar/Navbar.css';
 import DesktopLogo from '../../../assets/brand/MemeGen-M-bg_removed-cropped.png';
+import TabletLogo from '../../../assets/brand/Tab_Logo-removebg.png'
 import {Link} from 'react-router-dom';
-
+import { FaAlignJustify } from "react-icons/fa6";
+import { IoClose} from "react-icons/io5";
+import { useState} from 'react';
 
 export default function Navbar() {
+  const[menuOpen,setMenuOpen]=useState(false);
+  const[pic,setPic]=useState(<FaAlignJustify/>);
+ 
+  const menu=()=>{
+    if(menuOpen){
+      setMenuOpen(!menuOpen);
+      setPic(<FaAlignJustify/>)
+   
+    }
+    else{
+      setMenuOpen(!menuOpen);
+      setPic(<IoClose/>)
+    }
+  }
+
   return (
     <div>
       <nav className='main-nav'>
-        <div className='sub-nav1'>
-            <button className='logo'>
-                <Link to="/"><img src={DesktopLogo} alt="website logo"/></Link>     
-            </button>
+        <div className='logo'>
+          
+                <Link to="/">
+                  <picture>
+                    <source  srcSet={TabletLogo} media='(max-width:700px)'/>
+                    <img src={DesktopLogo} alt="website logo"/>
+                   
+                  </picture>
+                </Link>     
+         
         </div>
-        <div className='nav-item'>
-            <ul className='nav-link'>
+        <div className='hamburger-menu'>
+            <button onClick={()=>{menu()}}>{pic}</button>
+        </div>
+        
+            <ul className= {menuOpen?"open":""}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
-        </div>
+      
       </nav>
     </div>
   )
